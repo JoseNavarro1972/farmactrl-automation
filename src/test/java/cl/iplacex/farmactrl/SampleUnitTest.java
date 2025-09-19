8f5afb216e3f95337775f656600f79a03fd410a4
JoseNavarro1972 <njose3372@gmail.com>
2025-09-18 22:03:11 -0300

    chore: proyecto Maven Java 21 + JUnit 5

---
+++ src/test/java/cl/iplacex/farmactrl/SampleUnitTest.java
@@ -0,0 +1,11 @@
+package cl.iplacex.farmactrl;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class SampleUnitTest {
+    @Test
+    void shouldAddTwoNumbers() {
+        assertEquals(4, 2 + 2);
+    }
+}
